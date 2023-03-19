600800bf386b0853fc6b9b9af789d606b4e54097
Giovanni Costagliola <giovanni.costagliola@gmail.com>
2023-03-19 17:14:42 +0100

    Added Google Analytics

---
+++ assets/js/gtag.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-EK7TW9HX52"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-EK7TW9HX52');
+</script>
